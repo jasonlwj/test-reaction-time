ac0580eff9bc23c14dcc5ca6249a1b39bf727138
Jason Liew <jason.97.liew@gmail.com>
2021-01-27 19:23:20 +1100

    Upload files

---
+++ index.js
@@ -0,0 +1,46 @@
+let testStarted = false
+let startTime = null
+let finishTime = null
+let timer = null
+
+const clickarea = document.querySelector('.clickarea')
+const message = document.querySelector('.message')
+
+const randomNumber = (min, max, int = false) => {
+	return (int)
+		? Math.floor(Math.random() * (max - min + 1)) + min
+		: Math.random() * (max - min) + min
+}
+
+const turnGreen = () => {
+	clickarea.classList.remove('red')
+	clickarea.classList.add('green')
+	message.textContent = 'Click!'
+}
+
+const handleClick = () => {
+	if (!testStarted) {
+		const msUntilGreen = randomNumber(2, 4)
+		startTime = new Date()
+		finishTime = new Date(startTime.getTime() + (msUntilGreen * 1000))
+
+		clickarea.classList.add('red')
+		message.textContent = 'Wait for green...'
+		testStarted = true
+
+		timer = setTimeout(turnGreen, msUntilGreen * 1000)
+	} else {
+		testStarted = false
+
+		if (new Date() < finishTime) {
+			clearTimeout(timer)
+			clickarea.classList.remove('red')
+			message.textContent = 'Too soon!'
+		} else {
+			clickarea.classList.remove('green')
+			message.textContent = `${new Date() - finishTime}ms`
+		}
+	}
+}
+
+clickarea.addEventListener('mousedown', handleClick)
